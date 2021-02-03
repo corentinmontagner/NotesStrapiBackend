b3f0f5391941816bb7187d6369da61e7ed6a26f3
Corentin Montagner <corentin.montagner@ultimadisplays.eu>
2021-02-03 12:08:39 +0100

    Add notes

--- config/env/development/database.js
+++ config/env/development/database.js
@@ -4,13 +4,18 @@ module.exports = ({ env }) => ({
       default: {
         connector: 'mongoose',
         settings: {
-          host: env('DATABASE_HOST'),
-          port: env.int('DATABASE_PORT'),
-          database: env('DATABASE_NAME'),
-          username: env('DATABASE_USERNAME'),
-          password: env('DATABASE_PASSWORD'),
+          host: env('DATABASE_HOST', 'strapi-cluster.6w5kn.mongodb.net'),
+          srv: env.bool('DATABASE_SRV', true),
+          port: env.int('DATABASE_PORT', 27017),
+          database: env('DATABASE_NAME', 'backend'),
+          username: env('DATABASE_USERNAME', 'strapi'),
+          password: env('DATABASE_PASSWORD', 'od2gLoUif5UFLuZF'),
         },
-        options: {},
+        options: {
+          authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
+          ssl: env.bool('DATABASE_SSL', true),
+        },
       },
     },
   });
+  
