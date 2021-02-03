b4e9720552a2ca4fb736c8a77ad952421f8f9498
Corentin Montagner <corentin.montagner@ultimadisplays.eu>
2021-02-03 11:12:44 +0100

    Update database config

--- config/database.js
+++ config/database.js
@@ -4,16 +4,17 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'mongoose',
       settings: {
-        host: env('DATABASE_HOST', 'strapi-cluster.6w5kn.mongodb.net'),
-        srv: env.bool('DATABASE_SRV', true),
-        port: env.int('DATABASE_PORT', 27017),
-        database: env('DATABASE_NAME', 'backend'),
-        username: env('DATABASE_USERNAME', 'strapi'),
-        password: env('DATABASE_PASSWORD', 'od2gLoUif5UFLuZF'),
+        uri: env('DATABASE_URI')
+        //host: env('DATABASE_HOST', 'strapi-cluster.6w5kn.mongodb.net'),
+        //srv: env.bool('DATABASE_SRV', true),
+        //port: env.int('DATABASE_PORT', 27017),
+        //database: env('DATABASE_NAME', 'backend'),
+        //username: env('DATABASE_USERNAME', 'strapi'),
+        //password: env('DATABASE_PASSWORD', 'od2gLoUif5UFLuZF'),
       },
       options: {
-        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
-        ssl: env.bool('DATABASE_SSL', true),
+        //authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
+        ssl: true //env.bool('DATABASE_SSL', true),
       },
     },
   },
